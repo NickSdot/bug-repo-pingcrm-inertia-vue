ae43bd04a4760306eec61de5549ee15999e88fb6
NickSdot <catch.github.com@yannick-spang.de>
2021-07-15 15:03:44 +0800

    Update interia and inertia-vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import PortalVue from 'portal-vue'
 import { InertiaProgress } from '@inertiajs/progress'
-import { createInertiaApp } from '@inertiajs/inertia-vue'
+import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue'
 
 Vue.config.productionTip = false
 Vue.mixin({ methods: { route: window.route } })
 Vue.use(PortalVue)
 Vue.use(VueMeta)
+Vue.component('InertiaHead', Head)
+Vue.component('InertiaLink', Link)
 
 InertiaProgress.init()
 
